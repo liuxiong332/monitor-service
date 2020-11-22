439a4fdcb0bd3f45bbf14a01dddfb7f001c155a1
liuxiong <liuxiong332@163.com>
2020-11-22 12:48:30 +0800

    Add controller

--- src/main/java/xiong/monitor/controller/EventNotifierController.java
+++ src/main/java/xiong/monitor/controller/EventNotifierController.java
@@ -3,6 +3,7 @@ package xiong.monitor.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import xiong.monitor.util.OutputResult;
@@ -12,8 +13,8 @@ public class EventNotifierController {
     Logger logger = LoggerFactory.getLogger(EventNotifierController.class);
 
     @PostMapping("/sendFileEvent")
-    OutputResult<Object> sendEvent(@RequestParam String path) {
-        logger.info("Receive file event with path {}", path);
+    OutputResult<Object> sendEvent(@RequestBody String paths) {
+        logger.info("Receive file event with path {}", paths);
         return new OutputResult<>(null);
     }
 }
