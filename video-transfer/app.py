ef6ceec8a00a3a036827d255c13761c3a397d696
liuxiong <liuxiong332@163.com>
2021-05-31 15:44:52 +0800

    Add start deepstream url

--- video-transfer/app.py
+++ video-transfer/app.py
@@ -5,6 +5,7 @@ import os
 import time
 import minio
 import datetime
+import json
 
 app = Flask(__name__)
 
@@ -38,3 +39,22 @@ def transfer_video():
             "pictures", file_name, video_path,
         )
         return "http://12.168.1.152:9000/pictures/{}".format(file_name)
+
+
+@app.route("/startDS", methods=['POST'])
+def start_ds():
+    print('start deepstream')
+    device_info = request.get_json(force=True)
+    print(device_info)
+    subprocess.run(['pkill', 'deepstream-app'])
+    
+    with open('/home/optical/test_config/devices.json', 'w') as f:
+        f.write(json.dumps(device_info, indent=4))
+
+    subprocess.run(['/home/optical/test_config/test'], cwd='/home/optical/test_config/')
+
+    for index in range(1, len(device_info['source']) + 1):
+        model_file = '/home/optical/test_config/model_{}/deepstream_config.txt'.format(index)
+        log_fn = open('dslog_{}.log'.format(index), 'w')
+        subprocess.Popen(['/opt/nvidia/deepstream/deepstream-5.0/sources/objectDetector_Yolo/deepstream-app', '-c', model_file], stdout=log_fn, stderr=log_fn, cwd='/home/optical/test_config/')
+    return 'OK'
