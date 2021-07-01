23d4eedf187bcbf79f6820657c37af574a8626d6
liuxiong <liuxiong332@163.com>
2021-07-01 16:02:21 +0800

    Change port

--- webapp/src/common.js
+++ webapp/src/common.js
@@ -1,17 +1,21 @@
+const INNER_HOST = process.env.REACT_APP_INNER_HOST;
 
-const IS_INNER_NET = /172\.16\.\d+\.\d+/.test(window.location.host) // window.location.host
+const IS_INNER_NET = INNER_HOST ? new RegExp(INNER_HOST.replace('.', '\\.')).test(window.location.host) : false // window.location.host
 const IS_LOCAL_HOST = /localhost/.test(window.location.host)
 
 const BROWSER_DOMAIN = /([^:]+)/.exec(window.location.host)[0]
 
 const SERVICE_HOST = process.env.REACT_APP_SERVICE_HOST || BROWSER_DOMAIN;
-const SERVICE_PORT = process.env.REACT_APP_SERVICE_PORT || 8088;
+
+const LOCAL_SERVICE_PORT = process.env.REACT_APP_SERVICE_PORT || 8088
+const REMOTE_SERVICE_PORT = process.env.REACT_APP_REMOTE_SERVICE_PORT || 8088
+const SERVICE_PORT = IS_INNER_NET || IS_LOCAL_HOST ? LOCAL_SERVICE_PORT : REMOTE_SERVICE_PORT;
 
 const MINIO_HOST = process.env.REACT_APP_MINIO_HOST || BROWSER_DOMAIN;
 const MINIO_PORT = Number(process.env.REACT_APP_MINIO_PORT || 9000);
 
-const LOCAL_VIDEO_PORT = process.env.REACT_APP_LOCAL_VIDEO_PORT || 8080
-const REMOTE_VIDEO_PORT = process.env.REACT_APP_REMOTE_VIDEO_PORT || 8080
+const LOCAL_VIDEO_PORT = process.env.REACT_APP_LOCAL_VIDEO_PORT || 8090
+const REMOTE_VIDEO_PORT = process.env.REACT_APP_REMOTE_VIDEO_PORT || 8090
 
 export const Domain = `http://${SERVICE_HOST}:${SERVICE_PORT}`;
 export const VideoDomain = `http://${SERVICE_HOST}:${IS_INNER_NET || IS_LOCAL_HOST ? LOCAL_VIDEO_PORT : REMOTE_VIDEO_PORT}`;
